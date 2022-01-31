8f5b6020d8ef5b634e3ec24e1bda3bc760baa574
Taufiq Abdullah <taufiq.abdullah@erajaya.com>
2022-01-31 15:16:17 +0700

    init

---
+++ src/KeycloakApi.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace taufiqtab\keycloakapi;
+ 
+/**
+ * Basic Calculator.
+ * 
+ */
+class KeycloakApi
+{
+
+    public static function getRoles()
+    {
+        return "the Roles";
+    }
+
+}
